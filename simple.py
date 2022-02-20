cea4a4a6491e59e90d81112bf821fbfd07a17127
Hatim Muhammed <hatimmohammed369@gmail.com>
2022-02-20 16:12:29 +0200

    feat: 🎸 Added automated argument parsing
    Use Python's ArgumentParser class

--- simple.py
+++ simple.py
@@ -356,22 +356,23 @@ class Interpreter:
 
 ####################################################################################################
 
-# run
-from sys import argv
-if len(argv) == 3 and argv[1].lower() == '-f':
-    with open(argv[2], 'r') as source_file:
-        source = str(source_file.read())
-else:
-    source = """
-int x := 123;
-function f(const int x) -> int => {
+if __name__ == '__main__':
+    from argparse import ArgumentParser
 
-    return x;
+    parser = ArgumentParser()
 
-}
+    parser.add_argument('-s', '--source', help='A small code sample to execute')
+    parser.add_argument('-f', '--file', help='Source file')
 
-"""
-tokenizer = Tokenizer(source)
-for t in tokenizer:
-    print(t)
-print(tokenizer.indent_stack)
+    args = parser.parse_args()
+
+    if args.file:
+        with open(args.file, 'r') as source_file:
+            source = str( source_file.read() )
+    else:
+        source = args.source
+
+    tokenizer = Tokenizer(source)
+    
+    for token in tokenizer:
+        print(token)
