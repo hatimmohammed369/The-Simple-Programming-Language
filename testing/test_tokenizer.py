59079f714cafe41057ec7258e0ade7c654a6a49e
Hatim Muhammed <hatimmohammed369@gmail.com>
2022-02-20 16:42:00 +0200

    feat: 🎸 Added (testing) dir
    All tests will be placed there

---
+++ testing/test_tokenizer.py
@@ -0,0 +1,30 @@
+# Testing the whole tokenization process
+
+from unittest import TestCase, main
+from sys import path
+
+path.append('/home/hatim/Desktop/The-Simple-Programming-Language/simple.py')
+
+from simple import Tokenizer
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+
+    parser.add_argument('-s', '--source', help='A small code sample to execute')
+    parser.add_argument('-f', '--file', help='Source file')
+    parser.add_argument('-a', '--all', 'Run all tests')
+
+    args = parser.parse_args()
+
+    if args.file:
+        with open(args.file, 'r') as source_file:
+            source = str( source_file.read() )
+    else:
+        source = args.source
+
+    tokenizer = Tokenizer(source)
+    
+    for token in tokenizer:
+        print(token)
