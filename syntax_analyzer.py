147973692a41794420387c472b78eab504710b5e
Hatim Muhammed <hatimmohammed369@gmail.com>
2022-03-04 20:15:49 +0200

    Added a preliminary sketch of syntax analysis

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -1,11 +1,27 @@
 #!/usr/local/bin/python3.10
 
 # File syntax_analyzer.py
-# Perform syntax analysis, in other words, check if given source code is syntactically correct 
+# Perform syntax analysis, in other words, check if given source code is syntactically correct
+
+from argparse import ArgumentParser
+from tokenizer import Tokenizer, Token
+
+
+class SyntaxAnalyzer:
+    def __init__(self, tokenizer_object: Tokenizer):
+        if not tokenizer_object.done:
+            tokenizer_object.tokenize()
+        self.tokenizer: Tokenizer = tokenizer_object
+        self.done = False  # becomes True after analyze(self) is done
+
+    def analyze(self):
+        for token in self.tokenizer:
+            pass
+        self.done = True
+        return self
+
 
 if __name__ == "__main__":
-    from argparse import ArgumentParser
-    from tokenizer import Tokenizer
 
     parser = ArgumentParser()
 
@@ -20,7 +36,4 @@ if __name__ == "__main__":
     else:
         source = args.source
 
-    tokenizer = Tokenizer(source)
-
-    for token in tokenizer:
-        print(token)
+    SyntaxAnalyzer(Tokenizer(source)).analyze()
