acdc295ea2afda0bb57cff8cb007abe0e639187c
Hatim Muhammed <hatimmohammed369@gmail.com>
2022-03-06 19:56:28 +0200

    KEYWORDS tuple for code use

---
+++ const.py
@@ -0,0 +1,28 @@
+# KEYWORDS / OPERATORS / ...
+# You know, those sorts of things
+
+KEYWORDS = (
+    "var",
+    "const",
+    "int",
+    "real",
+    "string",
+    "bool",
+    "not",
+    "and",
+    "or",
+    "true",
+    "false",
+    "array",
+    "ref_of",
+    "fn",
+    "return",
+    "if",
+    "else",
+    "else_if",
+    "for",
+    "while",
+    "break",
+    "continue",
+    "pass",
+)
