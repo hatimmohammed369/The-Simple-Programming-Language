fcdaa9124331489c6046a9dad5017e2207a0372b
Hatim Muhammed <hatimmohammed369@gmail.com>
2022-03-07 08:37:40 +0200

    Intepreter Main File

---
+++ simple.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3.10
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    from tokenizer import Tokenizer
+
+    parser = ArgumentParser()
+
+    parser.add_argument("-s", "--source", help="A small code sample to execute")
+    parser.add_argument("-f", "--file", help="Source file")
+
+    args = parser.parse_args()
+
+    if args.file:
+        with open(args.file, "r") as source_file:
+            source = str(source_file.read())
+    else:
+        source = args.source
+
+    tokenizer = Tokenizer(source)
+
+    for token in tokenizer:
+        print(token)
