70cfd52156230b16eb9030d4ae9bbab6eb0ae944
Hatim Muhammed <hatimmohammed369@gmail.com>
2022-03-09 05:37:16 +0200

    Preparing for merging in branch (MVP-Grammar-Units-Sytnax-Analyzers)

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -1,21 +1,3 @@
-#!/usr/local/bin/python3.10
-
-from tokenizer import *
-from dataclasses import dataclass
-
-
-@dataclass(init=True, repr=True)
-class SyntaxToken:
-    value: str
-    next_expected: tuple[str]
-    context: tuple[str]
-
-
-CONTEXTS = (
-    SyntaxToken("define", next_expected=("NAME",), context=("CONST_VAR_DEFINTION",)),
-)
-
-
 class SyntaxAnalyzer:
     def __init__(self, tokenizer_object: Tokenizer):
         self.tokenizer = tokenizer_object
